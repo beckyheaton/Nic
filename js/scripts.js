267193b1daf7b8f4f9504bf574a4fce463557036
Becky Heaton <becky.a.heaton@outlook.com>
2024-07-19 22:05:20 +1000

    sky

--- js/scripts.js
+++
@@ -1,8 +0,0 @@
-function submitDrawing() {
-    const drawing = document.getElementById('history-drawing').value;
-    if (drawing.trim() === '') {
-        alert('Please describe your vision of history.');
-    } else {
-        alert('Thank you for your submission!');
-    }
-}
